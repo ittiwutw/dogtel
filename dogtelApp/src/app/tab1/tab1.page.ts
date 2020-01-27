fa0bb05c190ef22cfdba8ca86e1110885c95120f
ittiwut <ittiwut.wong@gmail.com>
2020-01-27 23:47:22 +0700

    Initial Project

---
+++ dogtelApp/src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  slideOpts = {
+    slidesPerView: 1.5,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    speed: 400,
+    autoplay: true
+  };
+  constructor() {}
+
+}
